6fedc3477bb047db0cb2eb88318e978c39647ab9
Soe Thandar Lwin <soethandarlwin@Soes-MacBook-Air.local>
2023-03-29 14:45:43 +0700

    Make Appbar responsive

--- src/components/Appbar.jsx
+++ src/components/Appbar.jsx
@@ -1,25 +1,53 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import { IconButton } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
 
-function Appbar() {
-    return (
-        <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static" sx={{backgroundColor: "black"}}>
+const navItems = ["Home", "About", "Contact"];
+
+function Appbar(props) {
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
+
+  return (
+    <Box sx={{ flexGrow: 1, paddingBottom: '2em' }}>
+      <AppBar
+        component="nav"
+        position="static"
+        sx={{ backgroundColor: "black" }}
+      >
         <Toolbar>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             My Website
           </Typography>
-          <Button color="inherit">HOME</Button>
-          <Button color="inherit">ABOUT</Button>
-          <Button color="inherit">CONTACT</Button>
+          <Box sx={{ display: { xs: 'none', md: 'block' } }}>
+            {navItems.map((item) => (
+              <Button key={item} sx={{ color: "#fff" }}>
+                {item}
+              </Button>
+            ))}
+          </Box>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ display: { xs: 'block', md: 'none' } }}
+          >
+            <MenuIcon />
+          </IconButton>
         </Toolbar>
       </AppBar>
     </Box>
-    );
+  );
 }
 
 export default Appbar;
